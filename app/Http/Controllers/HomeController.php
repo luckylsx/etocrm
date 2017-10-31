43cd5237ef0a46b3067e71aba480fa9050293a6e
lsx <lucky9303@163.com>
2017-10-31 22:12:28 +0800

    get userId

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,6 @@ class Homecontroller extends Controller
         $url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token={$access_token}&code={$code}";
         $resData = httpGet($url);
         $res = json_decode($resData,true);
-        p($res);
         echo "该用户的userId是".array_get($res,'UserId') . "，user_ticket是：".$res['user_ticket'];
     }
 }
